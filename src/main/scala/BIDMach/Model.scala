8538f681d2b311da71705644eb201e74339f99aa
jcanny <jfc@cs.berkeley.edu>
2012-11-03 22:19:29 -0700

    added new model files

---
+++ src/main/scala/BIDMach/Model.scala
@@ -0,0 +1,55 @@
+package BIDMach
+import BIDMat.{Mat,BMat,CMat,DMat,FMat,IMat,HMat,GMat,GIMat,GSMat,SMat,SDMat}
+import BIDMat.MatFunctions._
+import BIDMat.SciFunctions._
+
+abstract class Model(data0:Mat, target0:Mat, opts:Model.Options) {
+  
+  def initmodel(data:Mat, target:Mat):Mat
+  
+  def gradfun(data:Mat, target:Mat):Double 
+
+}
+
+
+object Model {
+	class Options {
+  }
+	
+  def checkSize(a:Mat, nr:Int, nc:Int, b:Mat):Mat = {
+    if (a.asInstanceOf[AnyRef] != null && a.nrows == nr && a.ncols == nc) {
+      a
+    } else {
+      b.zeros(nr, nc)
+    }
+  }
+  
+  def checkSize(a:Mat, b:Mat):Mat = checkSize(a, b.nrows, b.ncols, b)
+  
+  def fsqrt(v:Float):Float = math.sqrt(v).asInstanceOf[Float]
+  
+  def mapfun2x2(fn:(Float, Float)=>(Float, Float), in0:FMat, in1:FMat, out0:FMat, out1:FMat) = {
+    if (in0.nrows != in1.nrows || in0.nrows != out0.nrows || in0.nrows != out1.nrows ||
+        in0.ncols != in1.ncols || in0.ncols != out0.ncols || in0.ncols != out1.ncols) {
+      throw new RuntimeException("dimensions mismatch")
+    }
+    var i = 0
+    while (i < in0.length) {
+      val (v1, v2) = fn(in0.data(i), in1.data(i))
+      out0.data(i) = v1
+      out1.data(i) = v2
+      i += 1
+    }
+  }
+  def mapfun2x1(fn:(Float, Float)=>Float, in0:FMat, in1:FMat, out0:FMat) = {
+    if (in0.nrows != in1.nrows || in0.nrows != out0.nrows ||
+        in0.ncols != in1.ncols || in0.ncols != out0.ncols) {
+      throw new RuntimeException("dimensions mismatch")
+    }
+    var i = 0
+    while (i < in0.length) {
+      out0.data(i) = fn(in0.data(i), in1.data(i))
+      i += 1
+    }
+  }
+}
