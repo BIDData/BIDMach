49b7a859b7189127462d77bedf06213d0b952a3f
jcanny <jfc@cs.berkeley.edu>
2013-07-10 01:12:05 -0700

    changes to datasource

--- src/main/scala/BIDMach/DataSource.scala
+++ src/main/scala/BIDMach/DataSource.scala
@@ -2,6 +2,7 @@ package BIDMach
 import BIDMat.{Mat,BMat,CMat,CSMat,DMat,FMat,IMat,HMat,GMat,GIMat,GSMat,SMat,SDMat}
 import BIDMat.MatFunctions._
 import BIDMat.SciFunctions._
+import scala.actors._
 
 abstract class DataSource(opts:DataSource.Options = new DataSource.Options) { 
   val options = opts
@@ -17,7 +18,7 @@ class MatDataSource(mats:Array[Mat], opts:DataSource.Options = new DataSource.Op
   val blockSize = opts.blockSize
   var omats:Array[Mat] = null
   
-  def init = {
+  def init() = {
     here = 0
     omats = new Array[Mat](mats.length)
     for (i <- 0 until mats.length) {
@@ -44,7 +45,8 @@ class MatDataSource(mats:Array[Mat], opts:DataSource.Options = new DataSource.Op
 
 }
 
-class FilesDataSource(fnames:Array[CSMat], opts:FilesDataSource.Options = new FilesDataSource.Options) extends DataSource(opts) { 
+class FilesDataSource(fnames:CSMat, dirname:(Int)=>String, ndirs:Int, 
+		opts:FilesDataSource.Options = new FilesDataSource.Options) extends DataSource(opts) { 
   val sizeMargin = opts.sizeMargin
   val blockSize = opts.blockSize
   var fileno = 0
@@ -54,13 +56,13 @@ class FilesDataSource(fnames:Array[CSMat], opts:FilesDataSource.Options = new Fi
   var ready:IMat = null
   
   def init = {
-    fileno = 0
-    colno = 0
+    fileno = 0                                                // Number of the current output file
+    colno = 0                                                 // Column number in the current output file
     omats = new Array[Mat](fnames.size)
-    matqueue = new Array[Array[Mat]](fnames.size)
-    ready = IMat(fnames.size, opts.lookahead)
+    matqueue = new Array[Array[Mat]](fnames.size)             // Queue of matrices for each output matrix
+    ready = IMat(opts.lookahead, 1)                           // Numbers of files currently loaded in queue
     for (i <- 0 until fnames.size) {
-      val mm = HMat.loadMat(fnames(i)(0))
+      val mm = HMat.loadMat(dirname(0) + fnames(i))
       omats(i) = mm match {
       case mm:SMat => SMat(mm.nrows, blockSize, (mm.nnz * sizeMargin * blockSize / mm.ncols).toInt)
       case mm:SDMat => SDMat(mm.nrows, blockSize, (mm.nnz * sizeMargin * blockSize / mm.ncols).toInt)
@@ -68,40 +70,57 @@ class FilesDataSource(fnames:Array[CSMat], opts:FilesDataSource.Options = new Fi
       } 
       matqueue(i) = new Array[Mat](opts.lookahead)
     }
+    for (i <- 0 until opts.lookahead) {
+      Actor.actor {
+        prefetch(i)
+      }
+    }
   }
   
   def next:Array[Mat] = {
     val filex = fileno % opts.lookahead
     var donextfile = false
     var todo = 0
+    while (ready(filex) < fileno) Thread.`yield`
     for (i <- 0 until fnames.size) {
-    	while (ready(i, filex) == 0) Thread.`yield`
-    	val matq = matqueue(i)(filex)
-    	val ccols = math.min(colno + blockSize, matq.ncols)
-    	omats(i)(?, 0 -> (ccols - colno)) = matq(?, colno -> ccols)
-    	if (ccols - colno < blockSize) {
-    	  ready(i, filex) = 0
-    	  todo = blockSize - ccols + colno
-    	  donextfile = true
-
-    	  if (fileno+1 < fnames(0).length) {
-    	    val filey = filex + 1
+      val matq = matqueue(i)(filex)
+      val ccols = math.min(colno + blockSize, matq.ncols)
+      omats(i) = matq(?, colno -> ccols)
+      if (ccols - colno <= blockSize) {
+    	todo = blockSize - ccols + colno
+    	donextfile = true
+    	if (todo > 0) {
+    	  if (fileno+1 < ndirs) {
+    	    val filey = (filex + 1) % opts.lookahead
+    	    while (ready(filey) < fileno+1) Thread.`yield`
     	    val matq = matqueue(i)(filey)
-    	    while (ready(i, filey) == 0) Thread.`yield`
-    	    omats(i)(?, (ccols - colno) -> blockSize) = matq(?, 0 -> todo)
-    	  }
+    	    omats(i) = omats(i) \ matq(?, 0 -> todo)
+    	  } 
     	}
+      } 
     }
     if (donextfile) {
       colno = todo
+      fileno += 1
     } else {
-    	colno += blockSize
+      colno += blockSize
     }
     omats
   }
   
+  def prefetch(ifile:Int) = {
+	ready(ifile) = ifile - opts.lookahead
+	for (inew <- ifile until ndirs by opts.lookahead) {
+	  while (ready(ifile) >= fileno) Thread.`yield`
+	  for (i <- 0 until fnames.size) {
+	    matqueue(i)(ifile) = HMat.loadMat(dirname(inew) + fnames(i))
+	  }
+	  ready(ifile) = inew
+	}
+  }
+  
   def hasNext:Boolean = {
-    true
+    (fileno < ndirs)
   }
 }
 
