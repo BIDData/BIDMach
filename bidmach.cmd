0b4f40bdea5486288da7f7da1819228875a3f614
jcanny <jfc@cs.berkeley.edu>
2013-08-20 13:05:43 -0700

    fix win script

--- bidmach.cmd
+++ bidmach.cmd
@@ -7,13 +7,13 @@
 SET JCUDA_VERSION=0.5.0
 SET LIBDIR="%CD%\..\BIDMat\lib"
 SET JCUDA_LIBDIR=%LIBDIR%
-SET PATH=%LIBDIR%\win64;%LIBDIR%\win64\JCUDA5.0;%PATH%
+SET PATH=%LIBDIR%;%PATH%
 
-SET BIDMACH_LIBS=%LIBDIR%\..\BIDMat.jar;%CD%\BIDMach.jar;%LIBDIR%\ptplot.jar;%LIBDIR%\ptplotapplication.jar;%LIBDIR%\jhdf5.jar;%LIBDIR%\commons-math3-3.1.1.jar
+SET BIDMACH_LIBS=%LIBDIR%\..\BIDMat.jar;%CD%\BIDMach.jar;%LIBDIR%\ptplot.jar;%LIBDIR%\ptplotapplication.jar;%LIBDIR%\jhdf5.jar;%LIBDIR%\commons-math3-3.1.1.jar;%LIBDIR%\lz4-1.1.2.jar
 
 SET JCUDA_LIBS=%JCUDA_LIBDIR%\jcuda-%JCUDA_VERSION%.jar;%JCUDA_LIBDIR%\jcublas-%JCUDA_VERSION%.jar;%JCUDA_LIBDIR%\jcufft-%JCUDA_VERSION%.jar;%JCUDA_LIBDIR%\jcurand-%JCUDA_VERSION%.jar;%JCUDA_LIBDIR%\jcusparse-%JCUDA_VERSION%.jar
 
 SET ALL_LIBS=%BIDMACH_LIBS%;%JCUDA_LIBS%;%JAVA_HOME%\lib\tools.jar
-echo %ALL_LIBS%
+:: echo %ALL_LIBS%
 
 scala -nobootcp -cp "%ALL_LIBS%" -Yrepl-sync -i %LIBDIR%\bidmat_init.scala
