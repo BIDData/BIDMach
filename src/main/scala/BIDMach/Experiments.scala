f874e9f13bfdf61ae74f1f58a3c4f15c124881d4
jcanny <jfc@cs.berkeley.edu>
2013-09-05 23:46:00 -0700

    parallel logistic regression

--- src/main/scala/BIDMach/Experiments.scala
+++ src/main/scala/BIDMach/Experiments.scala
@@ -195,7 +195,7 @@ object Twitter {
 	  val nuni0 = 50
 	  val nbi0 = 100
 	  val ntri0 = 200
-	  val ds = SFilesDataSource.twitterNgrams(nstart0, nend0)
+	  val ds = SFilesDataSource.twitterNgramBlend(nstart0, nend0)
 //	  val ds = SFilesDataSource.twitterWords(nstart0, nend0)
 	  ds.opts.addConstFeat = true
 	  ds.opts.featType = 0
