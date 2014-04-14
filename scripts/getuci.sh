076a81adf8f703250094c0b917d96696d0a723c1
jcanny <jfc@cs.berkeley.edu>
2014-04-14 10:40:30 -0700

    fixes to uci pull

--- scripts/getuci.sh
+++ scripts/getuci.sh
@@ -23,7 +23,10 @@ if [ ! -e "${1}.smat.lz4" ]; then
         echo "Uncompressing docword.${1}.txt.gz"
         gunzip -c "docword.${1}.txt.gz" | tail -n +4 > "docword.${1}.txt"
     fi
-    bidmach "BIDMach.NYTIMES.preprocess(\"${UCI}/\",\"${1}\")"
+    if [ ! -e "cols.imat.gz" ]; then
+        ${BIDMACH_SCRIPTS}/../bin/tparse.exe -i "docword.${1}.txt" -f "${UCI}/fmt.txt" -o "./${1}." -m "./${1}." -d " " -c
+    fi
+#    bidmach "BIDMach.NYTIMES.preprocess(\"${UCI}/\",\"${1}\")"
 fi
 
 if [ -e "docword.${1}.txt" ]; then
