0601847addf5e55c831dbda30977211407683728
jcanny <jfc@cs.berkeley.edu>
2014-05-01 10:59:26 -0700

    updates to benchmarks

---
+++ scripts/benchmarks/testVWLDA.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+time /code/vowpal_wabbit/vowpalwabbit/vw --lda 256 --lda_D 100000 --passes 3 --readable_model wordTopics.dat --bit_precision 10 --learning_rate 1.0 --lda_rho 0.1 --cache_file vw.cache --data /big/RCV1/v2/vw_dense_train.dat --lda_alpha 0.1 --random_weights true --power_t 0.5 --minibatch 1024 --initial_t 1.0 
+
+# BIDMach options
+#   opts.putBack = 1
+#   opts.uiter = 1
+#   opts.batchSize = 1024
+#   opts.npasses = 3
+#   opts.dim = 256
