9bd3b50b277b5d89d07fbfb707833969525adf7b
jcanny <jfc@cs.berkeley.edu>
2014-11-20 17:36:49 -0800

    fixed bidmat.cmd

--- bidmach.cmd
+++ bidmach.cmd
@@ -7,13 +7,12 @@
 SET JCUDA_VERSION=0.6.5
 SET LIBDIR=%CD%\lib
 SET JCUDA_LIBDIR=%LIBDIR%
-SET PATH=%LIBDIR%;%PATH%
 
 SET BIDMACH_LIBS=%LIBDIR%\BIDMat.jar;%CD%\BIDMach.jar;%LIBDIR%\ptplot.jar;%LIBDIR%\ptplotapplication.jar;%LIBDIR%\jhdf5.jar;%LIBDIR%\commons-math3-3.1.1.jar;%LIBDIR%\lz4-1.1.2.jar
 
 SET JCUDA_LIBS=%JCUDA_LIBDIR%\jcuda-%JCUDA_VERSION%.jar;%JCUDA_LIBDIR%\jcublas-%JCUDA_VERSION%.jar;%JCUDA_LIBDIR%\jcufft-%JCUDA_VERSION%.jar;%JCUDA_LIBDIR%\jcurand-%JCUDA_VERSION%.jar;%JCUDA_LIBDIR%\jcusparse-%JCUDA_VERSION%.jar
 
 SET ALL_LIBS=%LIBDIR%\IScala.jar;%BIDMACH_LIBS%;%JCUDA_LIBS%;%JAVA_HOME%\lib\tools.jar
-SET JAVA_NATIVE='-Djava.library.path='%LIBDIR%;%PATH%
+(SET JAVA_OPTS=-Djava.library.path="%LIBDIR%;%PATH%" %JAVA_OPTS%)
 
 %CD%\scripts\scala\scala.bat -toolcp "%ALL_LIBS%" -Yrepl-sync -i %LIBDIR%\bidmach_init.scala
