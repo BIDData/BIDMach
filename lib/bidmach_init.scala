1c230a3cf211606287615c1a8fe8a9b466866b3a
jcanny <jfc@cs.berkeley.edu>
2015-01-29 00:00:02 -0800

    import GLMat

--- lib/bidmach_init.scala
+++ lib/bidmach_init.scala
@@ -1,4 +1,4 @@
-import BIDMat.{CMat,CSMat,DMat,Dict,IDict,Image,FMat,FND,GDMat,GMat,GIMat,GSDMat,GSMat,HMat,IMat,LMat,Mat,SMat,SBMat,SDMat}
+import BIDMat.{CMat,CSMat,DMat,Dict,FMat,FND,GMat,GDMat,GIMat,GLMat,GSMat,GSDMat,HMat,IDict,Image,IMat,LMat,Mat,SMat,SBMat,SDMat}
 import BIDMat.MatFunctions._
 import BIDMat.SciFunctions._
 import BIDMat.Solvers._
