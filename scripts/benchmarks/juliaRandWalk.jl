c751b3827a595c38348b70cd94c7ba6eca21083e
John Canny <jfc@cs.berkeley.edu>
2015-03-17 19:29:17 -0700

    added BIDMat tutorial

--- scripts/benchmarks/juliaRandWalk.jl
+++ scripts/benchmarks/juliaRandWalk.jl
@@ -1,8 +1,16 @@
 
-function rw(n::Int32)
+function rw(n::Int64)
     s::Float32 = 0
     for i = 1:n
         s += rand()
     end
     s
+end
+
+function fib(n::Int64)
+  if (n <= 2) 
+     1
+  else
+     fib(n-1) + fib(n-2)
+  end
 end
