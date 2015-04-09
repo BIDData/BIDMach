87212b401de4c700e24c69f0f6e54fbf7a47c61d
jcanny <jfc@cs.berkeley.edu>
2015-04-08 23:51:34 -0700

    CPU word2vec kernels

--- jni/src/BIDMach_CPUMACH.c
+++ jni/src/BIDMach_CPUMACH.c
@@ -2,6 +2,75 @@
 #include <mkl.h>
 #include <omp.h>
 
+JNIEXPORT void JNICALL Java_edu_berkeley_bid_CPUMACH_word2vec
+(JNIEnv *env, jobject obj, jint nrows, jint ncols, const jint nwa, const jint nwb, jintArray jWA, jintArray jWB, 
+ jfloatArray jA, jfloatArray jB, jfloat lrate)
+{
+  int i, j, k, c, ia, ib, coff;
+  float cv;
+  int * WA = (jint *)((*env)->GetPrimitiveArrayCritical(env, jWA, JNI_FALSE));
+  int * WB = (jint *)((*env)->GetPrimitiveArrayCritical(env, jWB, JNI_FALSE));
+  float * A = (jfloat *)((*env)->GetPrimitiveArrayCritical(env, jA, JNI_FALSE));
+  float * B = (jfloat *)((*env)->GetPrimitiveArrayCritical(env, jB, JNI_FALSE));
+  float * C;
+
+  C = (float *)malloc(nwa*nwb*sizeof(float));
+  
+
+#pragma omp parallel for
+  for (i = 0; i < ncols; i++) {
+    for (j = 0; j < nwa; j++) {
+      ia = nrows*WA[j+i*nwa];
+      for (k = 0; k < nwb; k++) {
+        ib = nrows*WB[k+i*nwb];
+        cv = 0;
+        for (c = 0; c < nrows; c++) {
+          cv += A[c + ia] * B[c + ib];
+        }
+
+        if (cv > 16.0f) {
+          cv = 1.0f;
+        } else if (cv < -16.0f) {
+          cv = 0.0f;
+        } else {
+          cv = exp(cv);
+          cv = 1.0f / (1.0f + cv);
+        }
+
+        C[j + k * nwa] = -cv;
+      }
+    } 
+
+    for (j = 0; j < nwa; j++) {
+      ia = nrows*WA[j+i*nwa];
+      for (k = 0; k < nwb; k++) {
+        ib = nrows*WB[k+i*nwb];
+        cv = lrate * C[j + nwa * k];
+        for (c = 0; c < nrows; c++) {
+          A[c + ia] += cv * B[c + ib];
+        }
+      }
+    }
+
+    for (k = 0; k < nwb; k++) {
+      ib = nrows*WB[k+i*nwb];
+      for (j = 0; j < nwa; j++) {
+        ia = nrows*WA[j+i*nwa];
+        cv = lrate * C[j + nwa * k];
+        for (c = 0; c < nrows; c++) {
+          B[c + ib] += cv * A[c + ia];
+        }
+      }
+    }
+  }
+
+  free(C);
+  (*env)->ReleasePrimitiveArrayCritical(env, jB, B, 0);
+  (*env)->ReleasePrimitiveArrayCritical(env, jA, A, 0);
+  (*env)->ReleasePrimitiveArrayCritical(env, jWB, WB, 0);
+  (*env)->ReleasePrimitiveArrayCritical(env, jWA, WA, 0);
+}
+
 JNIEXPORT void JNICALL Java_edu_berkeley_bid_CPUMACH_word2vecFwd
 (JNIEnv *env, jobject obj, jint nrows, jint ncols, const jint nwa, const jint nwb, jintArray jWA, jintArray jWB, 
  jfloatArray jA, jfloatArray jB, jfloatArray jC)
