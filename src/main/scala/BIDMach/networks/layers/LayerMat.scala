9fd1668698897f6910c1b562a264040994028eff
jcanny <jfc@cs.berkeley.edu>
2016-02-04 15:18:50 -0800

    fix LSTM/SeqToSeq

--- src/main/scala/BIDMach/networks/layers/LayerMat.scala
+++ src/main/scala/BIDMach/networks/layers/LayerMat.scala
@@ -168,7 +168,7 @@ case class LayerMat(override val nrows:Int, override val ncols:Int, override val
 	}
 	
 	def backward(col1:Int, col2:Int, debug:Int, ipass:Int, ipos:Long) = {
-		for (i <- col2 to col2 by -1) {
+		for (i <- col2 to col1 by -1) {
 			for (j <- (nrows-1) to 0 by -1) {
 				if (debug > 0) {
 					println("  backward (%d,%d) %s" format (j, i, apply(j, i).getClass))
