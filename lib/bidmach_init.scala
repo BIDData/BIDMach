4a3ee567dcbf82f2a27e40eb56c93928ce3f01ac
jcanny <jfc@cs.berkeley.edu>
2016-03-19 23:56:27 -0700

    fixed startup script

--- lib/bidmach_init.scala
+++ lib/bidmach_init.scala
@@ -5,7 +5,7 @@ import BIDMat.Solvers._
 import BIDMat.Plotting._
 import BIDMach.Learner
 import BIDMach.models.{Click,FM,GLM,KMeans,KMeansw,LDA,LDAgibbs,Model,NMF,SFA,RandomForest,SVD}
-import BIDMach.networks.{DNN}
+import BIDMach.networks.{Net}
 import BIDMach.datasources.{DataSource,MatSource,FileSource,SFileSource}
 import BIDMach.datasinks.{DataSink,MatSink}
 import BIDMach.mixins.{CosineSim,Perplexity,Top,L1Regularizer,L2Regularizer}
