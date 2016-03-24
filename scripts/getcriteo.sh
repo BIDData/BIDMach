73bd94ae12a936311db56eff1cee221a2dcf4a15
John Canny <canny@berkeley.edu>
2016-03-24 00:38:16 -0700

    fixes to criterlr.ssc

--- scripts/getcriteo.sh
+++ scripts/getcriteo.sh
@@ -3,8 +3,8 @@
 cd ../data/criteo
 mkdir -p parts
 
-split -a 3 -d -l 500000 train.txt parts/train
-split -a 3 -d -l 500000 test.txt parts/test
+split -a 2 -d -l 500000 train.txt parts/train
+split -a 2 -d -l 500000 test.txt parts/test
 
 bidmach readcriteo.ssc
 
