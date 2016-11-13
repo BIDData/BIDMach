9b46adc0cf20efa4651ba21e8578888022b6edd8
Aleks Kamko <aykamko@gmail.com>
2016-11-13 04:10:41 +0000

    working parCall and GLM!!!

--- scripts/start_workers.sh
+++ scripts/start_workers.sh
@@ -5,7 +5,7 @@ while read worker_ip; do
     echo "Starting BIDMach worker on ${worker_ip}"
     ssh $SSH_OPTS "ec2-user@${worker_ip}" << EOS
 
-    nohup bidmach /opt/BIDMach/scripts/testrecv.ssc </dev/null >/tmp/bidmach_worker.log 2>&1 &
+    nohup bidmach /opt/BIDMach/scripts/testrecv.ssc </dev/null >/tmp/bidmach_worker.log 2>&1 & disown
 
 EOS
 done < /opt/spark/conf/slaves
