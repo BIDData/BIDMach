e373186d1d43f90232fb462ddc4f04a08311e4cc
Aleks Kamko <aykamko@gmail.com>
2016-11-22 02:34:00 +0000

    distributed rcv1 scripts

--- scripts/start_workers.sh
+++ scripts/start_workers.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 set -e
+
+if [[ -z $1 ]]; then
+  echo 'Must supply script argument for workers to start!' 1>&2
+  exit 1
+fi
+
+WORKER_SCRIPT="${PWD}/${1}"
+
 SSH_OPTS='-T -o ConnectTimeout=3'
 while read worker_ip; do
     echo "Starting BIDMach worker on ${worker_ip}"
     ssh $SSH_OPTS "ec2-user@${worker_ip}" << EOS
 
-    nohup bidmach /opt/BIDMach/scripts/testrecv.ssc </dev/null >/tmp/bidmach_worker.log 2>&1 & disown
+    nohup bidmach $WORKER_SCRIPT </dev/null >/tmp/bidmach_worker.log 2>&1 & disown
 
 EOS
 done < /opt/spark/conf/slaves
