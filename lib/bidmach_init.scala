a88b3efb1277b4e733b410704a14fc63795224a5
jcanny <jfc@cs.berkeley.edu>
2017-06-01 16:26:27 -0700

    added ref to BMat

--- lib/bidmach_init.scala
+++ lib/bidmach_init.scala
@@ -1,4 +1,4 @@
-import BIDMat.{CMat,CSMat,DMat,Dict,FMat,GMat,GDMat,GIMat,GLMat,GSMat,GSDMat,HMat,IDict,Image,IMat,LMat,Mat,SMat,SBMat,SDMat,TMat}
+import BIDMat.{BMat,CMat,CSMat,DMat,Dict,FMat,GMat,GDMat,GIMat,GLMat,GSMat,GSDMat,HMat,IDict,Image,IMat,LMat,Mat,SMat,SBMat,SDMat,TMat}
 import BIDMat.MatFunctions._
 import BIDMat.SciFunctions._
 import BIDMat.Solvers._
