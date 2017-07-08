f65f0d30cb8a472c833c0342a78a7c1bbb4c4eea
jcanny <jfc@cs.berkeley.edu>
2017-07-08 13:44:11 -0700

    added NetActor

---
+++ src/main/scala/BIDMach/networks/NetActor.scala
@@ -0,0 +1,101 @@
+package BIDMach.networks
+
+import BIDMat.{Mat,SBMat,CMat,CSMat,DMat,FMat,IMat,LMat,HMat,GFilter,GMat,GDMat,GIMat,GLMat,GSMat,GSDMat,JSON,ND,SMat,SDMat,TMat}
+import BIDMat.MatFunctions._
+import BIDMat.SciFunctions._
+import BIDMach.datasources._
+import BIDMach.datasinks._
+import BIDMach.updaters._
+import BIDMach.mixins._
+import BIDMach.models._
+import BIDMach._
+import BIDMach.networks.layers._
+import jcuda.jcudnn._
+import jcuda.jcudnn.JCudnn._
+import scala.collection.mutable.Set;
+import java.util.HashMap;
+import akka.actor.{Actor,Props,ActorSystem,ActorRef};
+
+/**
+ * Net compute Actor. Attach to the network to support async and GPU-streamed layer udpates.  
+ * 
+ * Call setup() once after creation to find outputs for each layer, and source and sink layers.
+ * 
+ * Supports forward and backward calls which block until the calculations are complete. 
+ * 
+ */
+
+class NetActor(val net:Net) extends Actor {
+  import LayerActor._
+  
+  val actors = net.layers.map((x)=>context.actorOf(LayerActor.props(x)));
+  val actorSet =  Set(actors:_*);
+  @volatile var todo = Set.empty[ActorRef];
+  var sources:Array[ActorRef] = null;
+  var sinks:Array[ActorRef] = null;
+  
+  def setup() = {
+    todo = actorSet.clone;
+    actors.foreach(_ ! GetOutputs);
+    waitComplete;
+    sources = actors.filter(_.asInstanceOf[LayerActor].ninputs == 0);
+    sinks = actors.filter(_.asInstanceOf[LayerActor].noutputs == 0).reverse;
+  }
+    
+  def forward() = {
+    prepForward();
+    doForward();
+  }
+  
+  def backward() = {
+    prepBackward();
+    doBackward();
+  }
+  
+  def receive = {  
+    case ImDone => {
+      todo.synchronized {
+      	todo -= sender();
+      }
+    }
+    case _ => {}
+  }
+  
+  def waitComplete = {
+  	var todoIsEmpty = false;
+    while (! todoIsEmpty) {
+    	todo.synchronized {
+      	todoIsEmpty = todo.isEmpty ;
+    	}
+      Thread.sleep(0,1000);
+    } 
+  }
+  
+  def prepForward() = {
+  	todo = actorSet.clone;
+    actors.foreach(_ ! PrepForward);
+    waitComplete;
+  }
+  
+  def doForward() = {
+    todo = actorSet.clone;
+    sources.foreach(_ ! DoForward);
+    waitComplete;
+  }
+  
+  def prepBackward() = {
+    todo = actorSet.clone;
+    actors.foreach(_ ! PrepBackward);
+    waitComplete;
+  }
+  
+  def doBackward() = {
+    todo = actorSet.clone;
+    sinks.foreach(_ ! DoBackward);
+    waitComplete;
+  }
+}
+
+object NetActor {
+  def props(net: Net): Props = Props(new NetActor(net));
+}
--- src/main/scala/BIDMach/networks/layers/LayerActor.scala
+++ src/main/scala/BIDMach/networks/layers/LayerActor.scala
@@ -16,75 +16,137 @@ import scala.collection.mutable.Set;
 import java.util.HashMap;
 import akka.actor.{Actor,Props,ActorSystem,ActorRef};
 
+/**
+ * Layer compute Actor. Attach one of these to each layer. 
+ * 
+ * On creation, this actor finds the set of Actors attached to this layer's inputs. 
+ * For this to work, actors should be created in the layer's DAG order. 
+ * 
+ * Send "GetOutputs" messages to all layer actors to find node successors. These messages can go in any order, and only 
+ * need to be sent once. 
+ * 
+ * Send "PrepForward" messages to all layer actors at the start of each forward cycle. 
+ * These messages can be sent in any order. 
+ * 
+ * Send "DoForward" messages to layer actors to start forward computation. Order can be arbitrary, but its generally 
+ * most efficient to send these in DAG order. These need only be sent to source (indegree zero) nodes. 
+ * 
+ * Send "PrepBackward" messages to all layer actors at the start of each backward cycle. 
+ * These messages can be sent in any order. 
+ * 
+ * Send "DoBackward" messages to layer actors to start backward computation. Order can be arbitrary, but latency is
+ * minimized when message are sent in reverse DAG order. These need only be sent to sink (outdegree zero) nodes. 
+ * 
+ * In all cases, "ImDone" messages are sent to the parent actor when each node has completed its task. 
+ * 
+ */
+
 class LayerActor(val layer:Layer) extends Actor {
   import LayerActor._
   layer.myActor = self;
-  val inputActors = layer.inputs.map(_.layer.myActor).distinct;
+  val inputActors = Set(layer.inputs.map(_.layer.myActor):_*);
   val ninputs = inputActors.size;
-  var outputActors = Set[ActorRef]();
+  var outputActors = Set.empty[ActorRef];
+  var todoInputs = Set.empty[ActorRef];
+  var todoOutputs = Set.empty[ActorRef];
   def noutputs = outputActors.size;
-  var recvdInputs = 0;
-  var recvdOutputs = 0;
   
   def receive = {
+
+    case GetOutputs => {
+      if (ninputs == 0) {
+      	context.parent ! ImDone;       
+      } else {
+      	todoInputs = inputActors.clone;
+      	for (a <- inputActors) {
+      		a ! IsOutput;
+      	}
+      }
+    }
+    
+    case IsOutput => {
+      outputActors += sender();
+      sender() ! AckIsOutput;
+    } 
+    
+    case AckIsOutput => {
+      todoInputs -= sender();
+      if (todoInputs.isEmpty) {
+      	context.parent ! ImDone;      
+      }
+    }
+    
+    case PrepForward => {
+    	if (ninputs > 0) {
+    		todoInputs = inputActors.clone;
+    	}
+    	context.parent ! ImDone;
+    }
+    
     case DoForward => {
-      recvdInputs = 0;
       if (ninputs == 0) {
         layer.forward;
         for (a <- outputActors) {
           a ! ReadyForward;
         }
+        context.parent ! ImDone;
       }
-      sender() ! ImDone;
     }
+    
     case ReadyForward => {
-      recvdInputs += 1;
-      if (recvdInputs == ninputs) {
+      todoInputs -= sender();
+      if (todoInputs.isEmpty) {
         layer.forward;
         for (a <- outputActors) {
           a ! ReadyForward;
         }
+        context.parent ! ImDone;
       }
     }
+    
+    case PrepBackward => {
+    	if (noutputs > 0) {
+    		todoOutputs = outputActors.clone;
+    	}
+    	context.parent ! ImDone;
+    }
+    
     case DoBackward => {
-      recvdOutputs = 0;
       if (noutputs == 0) {
         layer.backward;
         for (a <- inputActors) {
           a ! ReadyBackward;
         }
-      }  
-      sender() ! ImDone;
+        context.parent ! ImDone;
+      } 
     }
+    
     case ReadyBackward => {
-      recvdOutputs += 1;
-      if (recvdOutputs == noutputs) {
+      todoOutputs -= sender();
+      if (todoOutputs.isEmpty) {
         layer.backward;
         for (a <- inputActors) {
           a ! ReadyBackward;
         }
+        context.parent ! ImDone;
       }   
     }
-    case GetOutputs => {
-      for (a <- inputActors) {
-        a ! IsOutput;
-      }
-      sender() ! ImDone;
-    }
-    case IsOutput => {
-      outputActors.add(sender());
-    }    
+    
+
     case _ => {}
   }
 }
 
 object LayerActor {
-  case object DoForward;
-  case object DoBackward;
-  case object ReadyForward;
-  case object ReadyBackward;
   case object GetOutputs;
   case object IsOutput;
+  case object AckIsOutput
+  case object PrepForward;
+  case object DoForward;
+  case object ReadyForward;
+  case object PrepBackward;
+  case object DoBackward;
+  case object ReadyBackward;;
   case object ImDone;
   def props(layer: Layer): Props = Props(new LayerActor(layer));
 }
