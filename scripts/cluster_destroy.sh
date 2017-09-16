834ef7405ad2b84f769c4daf8965e24aaf457b0d
John Canny <canny@berkeley.edu>
2017-09-15 22:59:03 -0700

    added destroy cluster

--- scripts/cluster_destroy.sh
+++ scripts/cluster_destroy.sh
@@ -4,6 +4,6 @@ if [[ "$CLUSTER" == "" ]]; then
     CLUSTER="bidcluster1"
 fi
 
-echo "y" | python bidmach_ec2.py -k "dss2_rsa" -i ~/.ssh/dss2_rsa --region=us-west-2 destroy $CLUSTER
+python bidmach_ec2.py -k "dss2_rsa" -i ~/.ssh/dss2_rsa --region=us-west-2 destroy $CLUSTER
 
 
