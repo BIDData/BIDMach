12b0ea0ea3139c953a9d9c63d3c5163ebef464b5
John Canny <canny@berkeley.edu>
2017-09-16 21:30:37 -0700

    add tags to launch script

--- scripts/cluster_launch.sh
+++ scripts/cluster_launch.sh
@@ -5,4 +5,4 @@ if [[ "$CLUSTER" == "" ]]; then
 fi
 
 # launch a cluster
-python bidmach_ec2.py -k "dss2_rsa" -i ~/.ssh/dss2_rsa -a "ami-ba7281c2" -s 4 --instance-type=p2.xlarge --region=us-west-2 --zone=us-west-2a --vpc-id="vpc-c93fbdac" --subnet-id="subnet-75177210" launch $CLUSTER
+python bidmach_ec2.py -k "dss2_rsa" -i ~/.ssh/dss2_rsa -a "ami-ba7281c2" -s 4 --instance-type=p2.xlarge --region=us-west-2 --zone=us-west-2a --vpc-id="vpc-c93fbdac" --subnet-id="subnet-75177210" --additional-tags='Group:DSS 2' launch $CLUSTER
