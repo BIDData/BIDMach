53c8ee936f612de7148507657df0768c3936757d
John Canny <canny@berkeley.edu>
2017-09-23 18:38:52 -0700

    tweaks to logging

--- scripts/runall.sh
+++ scripts/runall.sh
@@ -2,5 +2,5 @@
 
 while read slave; do
     echo ssh "${slave}" "${1}"
-    ssh -n "${slave}" "${1}"
+    ssh -n -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "${slave}" "${1}"
 done < /code/BIDMach/conf/slaves
