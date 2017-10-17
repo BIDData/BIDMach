a1292141eeb0515502c559aad678d7901bfa89ca
Mick Jermsak Jermsurawong <mick@redmart.com>
2017-10-17 16:57:01 -0700

    fix: fix runtime class cast exception/serialization issues

--- src/main/scala/BIDMach/allreduce/GridMessages.scala
+++ src/main/scala/BIDMach/allreduce/GridMessages.scala
@@ -2,14 +2,8 @@ package BIDMach.allreduce
 
 import akka.actor.ActorRef
 
-//#messages
-// master messages
-final case class OrganizeGridWorker(text: String)
-final case class GridOrganizationFailed(reason: String, job: OrganizeGridWorker)
 
 // worker messages
-//final case class GridNeighborAddresses(addresses: Seq[ActorRef])
 final case class GridGroupAddresses(group: GridGroup, addresses: Set[ActorRef])
 final case class HelloFromGroup(text: String)
-case class GreetGroups()
-//#messages
+final case class GreetGroups()
