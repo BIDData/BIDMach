880f87242a8991d6cf5bf5d9a3c344db1e30427b
Kevin Peng <kpeng3.4@berkeley.edu>
2018-01-24 16:19:41 -0800

    Implement TRAIN/TEST functionality
    Now loadUntrainedModel and loadTrainedModel return CaffeModel objects.
    When predictors are generated, the CaffeModel object automatically swaps
    out the train-only layers for test-only layers.

--- src/main/scala/BIDMach/extern/CaffeModel.scala
+++ src/main/scala/BIDMach/extern/CaffeModel.scala
@@ -7,42 +7,70 @@ import BIDMach.datasources.DataSource
 import BIDMach.networks.Net
 import BIDMach.networks.layers._
 import scala.collection.JavaConversions._
+import scala.collection.generic.FilterMonadic
 import scala.collection.mutable
 import scala.language.implicitConversions
 import scala.Option
 import scala.util.control.Breaks._
 import java.io.InputStream
+import java.lang.IllegalArgumentException
 import _root_.caffe.Caffe
 import _root_.caffe.Caffe.LRNParameter.NormRegion
 import _root_.caffe.Caffe.PoolingParameter.PoolMethod
 import com.google.protobuf._
 import jcuda.jcudnn.cudnnPoolingMode
 
-object CaffeModel {
-  private class CaffeLayer(val param:Caffe.LayerParameter) {
-    val inputs = new mutable.ArrayBuffer[CaffeLayer]
-    var inodeFirst = -1
-    var inodeLast = -1
-  }
+class CaffeModel private(net:Net, netParam:Caffe.NetParameterOrBuilder, layers:Seq[CaffeLayer]) {
+  import CaffeModel._
 
-  def loadUntrainedModel(fin:Readable, net:Net):Unit = {
+  def predictor(data:Mat, labels:Mat) = {
+    val (nn, opts) = Net.predictor(net, data, labels)
+
+    val newNet = nn.model.asInstanceOf[Net]
+    // It's assumed that model layers between train and test are the same
+    val (_, testNodes, _) = parseProtobuf(netParam, Caffe.Phase.TEST, newNet)
+    newNet.opts.nodeset = new NodeSet(testNodes.toArray)
+    net.createLayers
+    net.layers.map((x:Layer) => if (x != null) x.getModelMats(net))
+    for (i <- 0 until net.modelmats.length) {
+      if (net.modelmats(i) ne null) net.modelmats(i) = net.convertMat(net.modelmats(i));
+    }
+    // TODO: do I have to evalbatch(gmats, 0, 0)?
+    
+    (nn, opts)
+  }
+}
+
+object CaffeModel {
+  def loadUntrainedModel(fin:Readable, net:Net) = {
     val caffeBuilder = Caffe.NetParameter.newBuilder()
     TextFormat.merge(fin, caffeBuilder)
-    parseProtobuf(caffeBuilder, net)
+
+    val (layers, nodes, _) = parseProtobuf(caffeBuilder, Caffe.Phase.TRAIN, net)
+    net.opts.nodeset = new NodeSet(nodes.toArray)
+
+    new CaffeModel(net, caffeBuilder, layers)
   }
-  
-  def loadTrainedModel(fin:InputStream, net:Net):Unit = {
+
+  def loadTrainedModel(fin:InputStream, net:Net) = {
     val cis = CodedInputStream.newInstance(fin)
     cis.setSizeLimit(1 << 30)
     val netParam = Caffe.NetParameter.parseFrom(cis)
-    parseProtobuf(netParam, net)
+
+    val (layers, nodes, modelMats) = parseProtobuf(netParam, Caffe.Phase.TEST, net)
+    net.opts.nodeset = new NodeSet(nodes.toArray)
+    net.setmodelmats(modelMats.toArray)
+    net.opts.nmodelmats = modelMats.length
+
+    new CaffeModel(net, netParam, layers)
   }
 
-  private def parseProtobuf(netParam:Caffe.NetParameterOrBuilder, net:Net) = {
+  private def parseProtobuf(netParam:Caffe.NetParameterOrBuilder, phase:Caffe.Phase, net:Net) = {
     // Caffe only supports CrossCorrelation convolution
     net.opts.convType = Net.CrossCorrelation
     
-    val layers = toposort(resolveLayerLinks(netParam.getLayerList()))
+    val layersForPhase = filterLayers(netParam.getLayerList(), phase)
+    val layers = toposort(resolveLayerLinks(layersForPhase))
 
     // TODO: enforce NCHW if necessary
     // Translate every layer and build a mapping of blobs to layers feeding into them
@@ -55,6 +83,7 @@ object CaffeModel {
       val layer = layers(i)
       var incr = 1
 
+      // Translate layer according to its layer type
       var (newNodes, newMats):(Array[Node],Seq[Mat]) = layer.param.getType() match {
         case "Convolution" => translateConvolution(layer, net)
         case "Pooling" => translatePooling(layer)
@@ -113,6 +142,7 @@ object CaffeModel {
         case unknownType => throw new NotImplementedError("\"%s\" is not implemented yet" format unknownType)
       }
       
+      // Set lr scaling and model mat for ModelNodes
       newNodes match {
         case Array(modelNode:ModelNode) => {
           if (layer.param.getParamCount() >= 1) {
@@ -142,19 +172,42 @@ object CaffeModel {
       i += incr
     }
 
-    net.opts.nodeset = new NodeSet(nodes.toArray)
-    if (modelMats.length > 0) {
-      net.setmodelmats(modelMats.toArray)
-      net.opts.nmodelmats = modelMats.length
+    (layers, nodes, modelMats)
+  }
+  
+  /** Filters out layers in the given {@code LayerParameter} sequence to only contain ones
+   *  wanted for the given {@code phase}.
+   */
+  private def filterLayers(layerList:Seq[Caffe.LayerParameter], phase:Caffe.Phase) = {
+    layerList.withFilter(layer => {
+      if (layer.getIncludeCount() > 0 && layer.getExcludeCount() > 0) {
+        throw new IllegalArgumentException(s"Layer ${layer.getName()}: only include rules xor exclude rules"
+                                           + "can be specified")
+      } else if (layer.getIncludeCount() > 0) {
+        layer.getIncludeList().exists(netStateRule => stateMatchesRule(netStateRule, phase))
+      } else {
+        !layer.getExcludeList().exists(netStateRule => stateMatchesRule(netStateRule, phase))
+      }
+    })
+  }
+  
+  private def stateMatchesRule(netStateRule:Caffe.NetStateRule, phase:Caffe.Phase) = {
+    var matches = true
+    for (fieldDesc <- netStateRule.getAllFields().keys) {
+      fieldDesc.getName() match {
+        case "phase" => matches &= netStateRule.getPhase() == phase
+        case _ => println(s"Warning: net state rule ${fieldDesc.getName()} is not implemented")
+      }
     }
+    matches
   }
   
   /** Creates a list of {@code Layer} objects, setting their {@code lowers} and {@code uppers}
    *  attributes based on their links to blobs in the protobuf.
    */
-  private def resolveLayerLinks(layerList:Seq[Caffe.LayerParameter]):Seq[CaffeLayer] = {
+  private def resolveLayerLinks(layers:FilterMonadic[Caffe.LayerParameter, Seq[Caffe.LayerParameter]]):Seq[CaffeLayer] = {
     val layerBuf = new mutable.ArrayBuffer[CaffeLayer]
-    for (layerParam <- layerList) {
+    for (layerParam <- layers) {
       layerBuf += new CaffeLayer(layerParam)
     }
     
@@ -475,4 +528,10 @@ object CaffeModel {
       new FMat(reverseDims, data).transpose((reverseDims.length - 1) to 0 by -1)
     }
   }
+}
+
+private class CaffeLayer(val param:Caffe.LayerParameter) {
+  val inputs = new mutable.ArrayBuffer[CaffeLayer]
+  var inodeFirst = -1
+  var inodeLast = -1
 }
