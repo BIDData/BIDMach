b500fd7cae72d84c29226c5a29cf96306b453c9b
John Canny <canny@berkeley.edu>
2019-03-06 19:34:21 -0800

    cluster login to slave or master with X windows

--- scripts/cluster_login.sh
+++ scripts/cluster_login.sh
@@ -4,8 +4,14 @@ if [[ "$CLUSTER" == "" ]]; then
     CLUSTER="bidcluster1"
 fi
 
+if [ ! ${1} == "" ]; then
+    LOGIN="-n ${1} login"
+else
+    LOGIN="login"
+fi
+
 # login to the master
-python bidmach_ec2.py -k "dss2_rsa" -i ~/.ssh/dss2_rsa --region=us-west-2 login $CLUSTER
+python bidmach_ec2.py -k "dss2_rsa" -i ~/.ssh/dss2_rsa --region=us-west-2 ${LOGIN} $CLUSTER
 
 
 
