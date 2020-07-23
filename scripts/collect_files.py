3db12a4f207047a230424c56d2302ee1fcd3226f
Bogdan Popescu <68062990+bopopescu@users.noreply.github.com>
2020-07-23 15:54:38 +0300

    Migrating from master/slave language

--- scripts/collect_files.py
+++ scripts/collect_files.py
@@ -7,17 +7,17 @@ import datetime
 
 def main():
     files = sys.argv[1:]
-    s = subprocess.check_output("python bidmach_ec2.py -k id_rsa -i ~/.ssh/id_rsa --region=us-west-2 get-slaves " + os.environ['CLUSTER'], shell=True)
-    slaves = s.splitlines()[2:]
+    s = subprocess.check_output("python bidmach_ec2.py -k id_rsa -i ~/.ssh/id_rsa --region=us-west-2 get-subordinates " + os.environ['CLUSTER'], shell=True)
+    subordinates = s.splitlines()[2:]
     dir = '/code/BIDMach/%s/%s' % (os.environ['CLUSTER'], datetime.datetime.now().strftime("%Y%m%d%H%M"))
     os.mkdir(dir)
-    for s in slaves:
-        slave_dir = '%s/%s' % (dir, s)
-        os.mkdir(slave_dir)
-        todostr = 'rsync -e "ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no" -avz ubuntu@%s:/code/BIDMach/logs/log.0.0.txt %s/' % (s, slave_dir)
+    for s in subordinates:
+        subordinate_dir = '%s/%s' % (dir, s)
+        os.mkdir(subordinate_dir)
+        todostr = 'rsync -e "ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no" -avz ubuntu@%s:/code/BIDMach/logs/log.0.0.txt %s/' % (s, subordinate_dir)
         print(todostr)
         subprocess.check_call(todostr, shell=True)
-        todostr = 'rsync -e "ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no" -avz ubuntu@%s:/code/BIDMach/scripts/logres* %s/' % (s, slave_dir)
+        todostr = 'rsync -e "ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no" -avz ubuntu@%s:/code/BIDMach/scripts/logres* %s/' % (s, subordinate_dir)
         print(todostr)
         subprocess.check_call(todostr, shell=True)
 
